835700d2063f98d1cdde80e0a7d0c83895e027c3
Jaylen Zeng <jzeng30@stuy.edu>
2023-05-22 14:56:10 -0400

    padding works!

--- MD5.c
+++ MD5.c
@@ -3,24 +3,25 @@
 #include <string.h>
 #include <stdint.h>
 
-void toBinary(int n) {
-  int a[10];
-  int i;
-  for(i=0; n>0; i++) {
-    a[i]=n%2;
-    n=n/2;
-  }
-  //printf("\nBinary of Given Number is=");
-  for(i=i-1; i>=0 ;i--) {
-    printf("%d",a[i]);
-  }
-  printf(" ");
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
 
+    for (i = size-1; i >= 0; i--) {
+        for (j = 7; j >= 0; j--) {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+        printf(" ");
+    }
+    puts("");
 }
 
-void printBinary(unsigned char* string) {
-  for (int i = 0; i < strlen(string); i++) {
-    toBinary(string[i]);
+void printBinary(unsigned char* string, int length) {
+  for (int i = 0; i < length; i++) {
+    //toBinary(string[i]);
   }
   printf("\n");
 }
@@ -29,13 +30,16 @@ void printBinary(unsigned char* string) {
 int main(int argc, char const *argv[]) {
   uint8_t* message = "test";
   int size = strlen(message) * 8;
-  printBinary(message);
+  printBits(strlen(message), message);
   //padding 448 mod 512 long
   int padding = 448 - size; //# of zeros were adding
   uint8_t* new_message;
   new_message = calloc(448, 1);
-  memcpy(new_message, message, size);
-  new_message[size] = 128;
-  printf("%02x\n", new_message);
+  for (int i = 0; i < strlen(message); i++) {
+    new_message[i] = message[i];
+  }
+  //memcpy(new_message, message, size);
+  new_message[strlen(message)] = 128;
+  printBits(448, new_message);
   return 0;
 }
